082e8cee0218b40749c028ec8037c95e154becd4
shalini <shalinimoorthy312@gmail.com>
2025-08-22 07:33:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.2 MB (33.02%)

---
+++ leetcode/0033-search-in-rotated-sorted-array/solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = size(nums) - 1;
+
+        while(left<=right)
+        {
+            int mid = (left+right) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[left] <= nums[mid])
+            {
+                if((nums[left] <= target) && (nums[mid] >= target))
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            else if(nums[right] >=nums[mid])
+            {
+                if(nums[mid]<= target && nums[right] >= target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+        
+    }
+};
