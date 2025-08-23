96733c3ee7f8919bc773781df3696602f5392bdf
shalini <shalinimoorthy312@gmail.com>
2025-08-23 13:49:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.4 MB (84.74%)

---
+++ leetcode/0069-sqrtx/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0) return 0;
+        int left = 1, right = x;
+        while(left <= right)
+        {
+            int mid = left + (right-left) /2;
+            long long squares = (long long) mid * mid;
+            if(squares==x) return mid;
+            if(squares<x)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return right;
+    }
+};
