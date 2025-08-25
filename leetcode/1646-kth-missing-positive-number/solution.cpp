65cf41942f758623ce54b817a4bacf4afb2cf767
shalini <shalinimoorthy312@gmail.com>
2025-08-25 09:30:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.4 MB (28.05%)

---
+++ leetcode/1646-kth-missing-positive-number/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+        int left = 0;
+        int right = n - 1;
+
+        while(left<=right)
+        {
+            int mid = (left+right) / 2;
+            int missing = arr[mid] - (mid + 1);
+            if(missing < k)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return (right + k + 1);
+        
+    }
+};
