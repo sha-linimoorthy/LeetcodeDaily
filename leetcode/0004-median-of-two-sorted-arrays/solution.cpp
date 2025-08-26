b9a748b4f6a9c08729c81fc5ae3f6deb4fd8cc4d
shalini <shalinimoorthy312@gmail.com>
2025-08-26 10:28:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 95.1 MB (80.18%)

---
+++ leetcode/0004-median-of-two-sorted-arrays/solution.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        int len = (n1+n2+1) / 2;
+        int left = 0;
+        int right = n1;
+        double answer;
+
+        while(left<=right)
+        {
+            int mid1 = (left+right) / 2;
+            int mid2 = len - mid1;
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
+            if(mid1 >= 1) l1 = nums1[mid1 - 1];
+            if(mid2 >= 1) l2 = nums2[mid2 - 1];
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            if(l1<=r2 && l2<=r1)
+            {
+                if((n1+n2)%2==0)
+                {
+                    answer = ((double) max(l1, l2) + (double) min(r1, r2) ) / 2.0;
+                }
+                else
+                {
+                    answer = (double) max(l1, l2);
+                }
+
+                break;
+            }
+            else if(l1 > r2)
+            {
+                right = mid1 - 1;
+            }
+            else
+            {
+                left = mid1 + 1;
+            }
+        }
+        return answer;
+    }
+};
