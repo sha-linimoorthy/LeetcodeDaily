45559247a0d9de258ac328cc0c9bb16a978669ff
shalini <shalinimoorthy312@gmail.com>
2025-08-27 14:47:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 50.7 MB (8.38%)

--- leetcode/2047-find-a-peak-element-ii/solution.cpp
+++ leetcode/2047-find-a-peak-element-ii/solution.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
-    vector<int> addElement(vector<int> &len, int i, int j)
+    int findMaxIndx(vector<vector<int>> & mat, int col)
     {
-        len[0] = i;
-        len[1] = j;
-        return len;
-    }
-    vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        vector<int> len(2,-1);
-        int row_size = mat.size() - 1;
-        int col_size = mat[0].size() - 1;
-        for(int i=0;i<=row_size;i++)
+        int max = INT_MIN;
+        int index = -1;
+        for(int i=0; i<mat.size(); i++)
         {
-            for(int j=0;j<=col_size;j++)
+            if(mat[i][col] > max) 
             {
-                int top = (i > 0)? mat[i-1][j] : -1;
-                int bottom = (i < row_size)? mat[i+1][j] : -1;
-                int right = (j < col_size)? mat[i][j+1] : -1;
-                int left = (j > 0)? mat[i][j-1] : -1;
-                if((mat[i][j] > top) && (mat[i][j] > bottom) && (mat[i][j] > left) && (mat[i][j] > right)) 
-                {
-                    return addElement(len, i, j);
-                }  
+                max = mat[i][col];
+                index = i;
             }
         }
-        return len;
+        return index;
+    }
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int row_size = mat.size();
+        int col_size = mat[0].size();
+        int left = 0;
+        int right = col_size - 1;
+
+        while(left<=right)
+        {
+            int mid = (left+right) / 2;
+            int row = findMaxIndx(mat, mid);
+            int left_val = (mid >= 1) ? mat[row][mid - 1] : -1;
+            int right_val = (mid < (col_size - 1)) ? mat[row][mid + 1] : -1;
+            if((mat[row][mid] > left_val) && (mat[row][mid] > right_val))
+            {
+                return {row, mid};
+            }
+            else if(left_val > mat[row][mid])
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+
+        }
+        return {-1, -1};
     }
 };
