9cad4238da4a3c68aa041506bc68e7224cf8f74e
shalini <shalinimoorthy312@gmail.com>
2025-09-09 12:23:04 +0000

    [LeetCode Sync] Runtime - 4 ms (55.12%), Memory - 11.8 MB (98.34%)

--- leetcode/0214-shortest-palindrome/solution.cpp
+++ leetcode/0214-shortest-palindrome/solution.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-    vector<int> longestPrefixSuffix(string s)
-    {
-        int n = s.length();
-        vector<int> lps(n,0);
-        int prevLps = 0, i = 1;
-        while(i<n)
-        {
-            if(s[prevLps]==s[i])
-            {
-                lps[i] = prevLps+1;
-                prevLps++; i++;
-            }
-            else if(prevLps==0)
-            {
-                lps[i]=0;
-                i++;
-            }
-            else
-            {
-                prevLps = lps[prevLps - 1];
-            }
-            
-        }
-        return lps;
-    }
     string shortestPalindrome(string s) {
-        string pattern = s;
-        reverse(pattern.begin(), pattern.end());
-        vector<int> lps = longestPrefixSuffix(s+"#"+pattern);
-        int indx = lps.back();
-        string str = s.substr(indx);
+        long long prefix = 0, suffix = 0, base = 29, power = 1;
+        int last_ind = -1;
+        int mod = 1e9+7;
+
+        for(int i=0; i<s.length(); i++)
+        {
+            int val = s[i] - '0' + 1;
+            prefix = (prefix * base) % mod;
+            prefix += val % mod;
+
+            suffix = (suffix + val * power) % mod;
+            power = (power * base) % mod;
+            if(prefix == suffix) 
+                last_ind = i;
+        }
+
+        string str = s.substr(last_ind+1);
         reverse(str.begin(), str.end());
         return str+s;
     }
