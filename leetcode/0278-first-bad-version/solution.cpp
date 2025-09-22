55a54ef2f6f7bda5b722cf117c4c7f28a87f00aa
shalini <shalinimoorthy312@gmail.com>
2025-09-22 05:30:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8 MB (37.42%)

--- leetcode/0278-first-bad-version/solution.cpp
+++ leetcode/0278-first-bad-version/solution.cpp
@@ -6,18 +6,18 @@ public:
     int firstBadVersion(int n) {
         int left = 1;
         int right = n;
-        while(left<right)
+        while(left<=right)
         {
             int mid = left + ((right - left) /2);
             if(isBadVersion(mid))
             {
-                right = mid;
+                right = mid - 1;
             }
             else
             {
                 left = mid + 1;
             }
         }
-        return right;
+        return left;
     }
 };
