012d1e04f2846c10b9da93cfcf1e25c04c483dd5
shalini <shalinimoorthy312@gmail.com>
2025-10-14 10:14:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 69.6 MB (26.03%)

--- leetcode/1046-max-consecutive-ones-iii/solution.cpp
+++ leetcode/1046-max-consecutive-ones-iii/solution.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
         int l = 0, r = 0;
-        int maxi = 0;
-        int kcopy = k;
-        while(r < nums.size()){
-            if(nums[r]==0) {
-                if(kcopy==0) continue;
-                else if(k==0){
-                    while(l<r && nums[l]==1) l++;
-                    l++;
-                }
-                else {
-                    k--;
-                }
+        int n = nums.size();
+        int zeroCount = 0;
+        int maxLen = 0;
+        while(r<n){
+            if(nums[r]==0) zeroCount++;
+            if(zeroCount > k) {
+                if(nums[l]==0) zeroCount--;
+                l++;
+            }
+            else{
+                maxLen = max(maxLen, r-l+1);
             }
             r++;
-            maxi = max(maxi, r-l); 
         }
-        maxi = max(maxi, r-l);
-        return maxi;
+        return maxLen;
     }
 };
